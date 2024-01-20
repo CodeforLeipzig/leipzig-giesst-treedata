b62224d8727ab8e88a525fb2cd8c0aab16754bee
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2024-01-20 21:10:31 +0100

    type safe csv read

--- treedata/radolan/write_radolan_geoarrow.py
+++ treedata/radolan/write_radolan_geoarrow.py
@@ -1,34 +1,34 @@
 import pandas as pd
 import geopandas as gpd
-import pyarrow as pa
 import pyarrow.feather as feather
 from lonboard._geoarrow.geopandas_interop import geopandas_to_geoarrow
 import pyarrow.parquet as pq
-from lonboard.colormap import apply_continuous_cmap
-from palettable.colorbrewer.diverging import RdYlGn_11
 
 
 def write_geoarrow_content(path, file_name):
-    df = pd.read_csv(f"{path}{file_name}.csv")
+    dtype_dic = {
+        'id': str,
+        'lng': float,
+        'lat': float,
+        'radolan_sum': int,
+        'age': int
+    }
+    df = pd.read_csv(f"{path}{file_name}.csv", dtype=dtype_dic)
     gdf = gpd.GeoDataFrame(
         df, geometry=gpd.points_from_xy(df.lng, df.lat)
     )
-    gdf = gdf.drop(columns=["lng", "lat", "radolan_sum"])
+    gdf = gdf.drop(columns=["lng", "lat"])
     table = geopandas_to_geoarrow(gdf, preserve_index=False)
-
-    age = gdf["age"]
-
-    colors = apply_continuous_cmap(age, RdYlGn_11)
-    table = table.append_column(
-        "colors", pa.FixedSizeListArray.from_arrays(colors.flatten("C"), 3)
-    )
-    table = table.drop_columns(columns=["age"])
 
     file_path = f"{path}{file_name}.feather"
     feather.write_feather(
-        table, file_path, compression="uncompressed"
+        table, file_path, compression=None
     )
-    pq.write_table(table, f"{path}{file_name}.parquet")
+    pq.write_table(
+        table,
+        f"{path}{file_name}.parquet",
+        compression=None
+    )
 
 
 def write_radolan_geoarrow(path):
