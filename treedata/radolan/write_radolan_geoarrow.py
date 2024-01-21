27829040d784d41e9522da282bb1ef53b58d61c1
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2024-01-21 14:44:11 +0100

    don't calculate color, will be done in frontend

--- treedata/radolan/write_radolan_geoarrow.py
+++ treedata/radolan/write_radolan_geoarrow.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import geopandas as gpd
-import pyarrow as pa
 import pyarrow.feather as feather
 from lonboard._geoarrow.geopandas_interop import geopandas_to_geoarrow
 import pyarrow.parquet as pq
-from lonboard.colormap import apply_continuous_cmap
-from palettable.colorbrewer.diverging import RdYlGn_11
 
 
 def write_geoarrow_content(path, file_name):
@@ -23,21 +20,6 @@ def write_geoarrow_content(path, file_name):
     gdf = gdf.drop(columns=["lng", "lat"])
     table = geopandas_to_geoarrow(gdf, preserve_index=False)
 
-    def get_color(row):
-        age = row["age"]
-        radolan_sum = row["radolan_sum"]
-        if age < 5:
-            return 300
-        elif age > 15:
-            return 300
-        else:
-            return radolan_sum
-    gdf['water_need'] = gdf.apply(get_color, axis=1)
-
-    colors = apply_continuous_cmap(gdf['water_need'], RdYlGn_11)
-    table = table.append_column(
-        "colors", pa.FixedSizeListArray.from_arrays(colors.flatten("C"), 3)
-    )
     file_path = f"{path}{file_name}.feather"
     feather.write_feather(
         table, file_path, compression="uncompressed"
