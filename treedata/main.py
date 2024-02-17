c012977b7215119c6ec29ee0c7b11ce3dceecf28
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2024-02-17 19:44:56 +0100

    chunk by process count

--- treedata/main.py
+++ treedata/main.py
@@ -7,36 +7,45 @@ from trees_shape import configure_trees_args as configure_trees_shape_args
 from trees_process import configure_trees_process_args
 from weather import configure_weather_args
 from dotenv import load_dotenv
+from multiprocessing import freeze_support
 
 
 ROOT_DIR = os.path.abspath(os.curdir)
-
-logger = logging.getLogger('root')
 FORMAT = "[%(levelname)s %(name)s] %(message)s"
-logging.basicConfig(format=FORMAT)
-logger.setLevel(logging.DEBUG)
-logging.getLogger("fiona.ogrext").setLevel(logging.WARNING)
 
-start = time.time()
-load_dotenv(f'{ROOT_DIR}/resources/.env')
 
-parser = argparse.ArgumentParser(description='Processing city shape, tree data and weather data')
-subparsers = parser.add_subparsers(help='actions', dest='action')
+def main():
+    freeze_support()
 
-trees_parser = subparsers.add_parser('trees', help="Download and process tree data")
-configure_trees_args(trees_parser)
+    logger = logging.getLogger('root')
+    logging.basicConfig(format=FORMAT)
+    logger.setLevel(logging.DEBUG)
+    logging.getLogger("fiona.ogrext").setLevel(logging.WARNING)
 
-trees_shape_parser = subparsers.add_parser('trees-shp', help="Download and process tree data from shapefile")
-configure_trees_shape_args(trees_shape_parser)
+    start = time.time()
+    load_dotenv(f'{ROOT_DIR}/resources/.env')
 
-trees_process_parser = subparsers.add_parser('trees_process', help="Transform and upload tree data")
-configure_trees_process_args(trees_process_parser)
+    parser = argparse.ArgumentParser(description='Processing city shape, tree data and weather data')
+    subparsers = parser.add_subparsers(help='actions', dest='action')
 
-weather_parser = subparsers.add_parser('weather', help="Download and process DWD radolan data")
-configure_weather_args(weather_parser)
+    trees_parser = subparsers.add_parser('trees', help="Download and process tree data")
+    configure_trees_args(trees_parser)
 
-res = parser.parse_args()
-res.func(res)
+    trees_shape_parser = subparsers.add_parser('trees-shp', help="Download and process tree data from shapefile")
+    configure_trees_shape_args(trees_shape_parser)
 
-end = time.time() - start
-logger.info("It took {} seconds to run the script".format(end))
+    trees_process_parser = subparsers.add_parser('trees_process', help="Transform and upload tree data")
+    configure_trees_process_args(trees_process_parser)
+
+    weather_parser = subparsers.add_parser('weather', help="Download and process DWD radolan data")
+    configure_weather_args(weather_parser)
+
+    res = parser.parse_args()
+    res.func(res)
+
+    end = time.time() - start
+    logger.info("It took {} seconds to run the script".format(end))
+
+
+if __name__ == "__main__":
+    main()
